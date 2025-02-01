60daa22b3c201f29575860717b8217b9a17188c2
Ali Zemani <ali.ze@arianatech.ir>
2025-02-01 16:51:37 +0330

    basic content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="flex flex-col">
-    <slot name="app-before" />
-    <div id="app-before" />
     <Header />
 
-    <main class="flex min-h-screen">
-      <div class="mx-auto">
-        <div class="prose md:prose-xl dark:prose-invert">
-          <UCard
-            :ui="{
-              root: 'ring-0 shadow-none bg-transparent',
-            }"
-          >
-            <slot />
-            <!-- Footer Slot -->
-            <template #footer>
-              ddd
-            </template>
-          </UCard>
-        </div>
-      </div>
-
-      <slot name="app-after" />
-      <div id="app-after" />
-    </main>
-
+    <UContainer class="prose md:prose-xl dark:prose-invert">
+      <UCard
+        :ui="{
+          root: 'ring-0 shadow-none bg-transparent',
+        }"
+      >
+        <slot />
+        <template #footer> ddd </template>
+      </UCard>
+    </UContainer>
   </div>
 </template>
