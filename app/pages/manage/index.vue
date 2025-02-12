bc784d793a8ea6d51780d83a612a891a1c657086
Ali Zemani <mehot1@gmail.com>
2025-02-12 14:20:39 +0330

    no ch

--- app/pages/manage/index.vue
+++ app/pages/manage/index.vue
@@ -6,10 +6,6 @@ definePageMeta({
 
 <template>
   <div class="flex flex-col">
-    <div class="flex w-full h-60">
-      <div class="basis-1/2"><ChartsSimple /></div>
-      <div class="basis-1/2"><ChartsSimple /></div>
-    </div>
     <div class="min-h-40">body</div>
   </div>
 </template>
