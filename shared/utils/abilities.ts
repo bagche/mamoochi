0403dc972255045ea161ca72910b502d4e6803f9
Ali Zemani <ali.ze@arianatech.ir>
2025-03-02 12:41:36 +0330

    ui cleanups

--- shared/utils/abilities.ts
+++ shared/utils/abilities.ts
@@ -1,5 +1,6 @@
 export const readDashboard = defineAbility((user: any) => {
-  return user?.permissions?.includes("dashboard.read") ?? false;
+  // return user?.permissions?.includes("dashboard.read") ?? false;
+  return true;
 });
 
 export const editPage = defineAbility((user: any) => {
