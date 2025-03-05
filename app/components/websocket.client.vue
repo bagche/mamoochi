4783d7ea6fe0134a549ff556cb4d245d436ee4c5
Ali Zemani <mehot1@gmail.com>
2025-03-05 22:49:33 +0330

    do ws?

---
+++ app/components/websocket.client.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useWebSocket } from "@vueuse/core";
+const wsProto = window.location.protocol === "https:" ? "wss:" : "ws:";
+const wsUrl = `${wsProto}//${window.location.host}/api/ws`;
+
+const { status, data, send, open, close } = useWebSocket(wsUrl);
+
+watch(data, () => {
+  console.log(data);
+});
+</script>
+<template>
+  {{ status }}
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="flex flex-col justify-start">
     <Header />
+    <Websocket />
     <div class="min-h-screen pb-10">
       <slot />
     </div>
     <Footer />
   </div>
 </template>
-<script setup lang="ts">
-import { useWebSocket } from "@vueuse/core";
-const wsProto = window.location.protocol === "https:" ? "wss:" : "ws:";
-const wsUrl = `${wsProto}//${window.location.host}/api/ws`;
-
-const { status, data, send, open, close } = useWebSocket(wsUrl);
-
-watch(status, () => {
-  console.log(status);
-});
-</script>
