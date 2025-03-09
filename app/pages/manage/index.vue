2e2438613883315b35a4d733dbc96821bfb4da23
Ali Zemani <mehot1@gmail.com>
2025-03-09 20:39:05 +0330

    chart clean

--- app/pages/manage/index.vue
+++ app/pages/manage/index.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 definePageMeta({
-  middleware: 'permissions',
-  permission: 'dashboard.read'
+  middleware: "permissions",
+  permission: "dashboard.read",
 });
 </script>
 
 <template>
   <div class="w-full min-h-screen flex-col">
     <UContainer>
-      <div class="max-w-7xl mx-auto flex flex-col items-center pt-10 px-4 gap-7">
-        <div class="w-full   flex  h-[calc(100vh-35rem)] ">
-          <ManageCostsChart class="basis-1/2" />
-          <ManageVisitorChart class="basis-1/2" />
+      <div
+        class="max-w-7xl mx-auto flex flex-col items-center pt-10 px-4 gap-7"
+      >
+        <div class="w-full flex h-[calc(100vh-35rem)]">
+          <ManageDashboardChart />
         </div>
         <ManageDashboardTabs />
       </div>
